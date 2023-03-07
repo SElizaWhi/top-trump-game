fc5d3cb9cad17d577adb7a85ce319284020dac4d
Sophie White <sophie.white@mordrid.net>
2023-03-07 12:55:26 +0100

    display only one card

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 class Card{
    
-    constructor(cardName, roles, awards, imdbScore, funFact){
-        this.cardName = cardName
-        this.roles = roles
-        this.awards = awards
-        this.imdbScore = imdbScore
-        this.funFact = funFact
-    } 
-    }
+  constructor(cardName, roles, awards, imdbScore, funFact){
+      this.cardName = cardName
+      this.roles = roles
+      this.awards = awards
+      this.imdbScore = imdbScore
+      this.funFact = funFact
+  } 
+  }
 
-const cards =[
+const deck =[
 new Card("George Clooney", 81, 95, 94, "Was voted `Sexiest Man Alive` by People magazine in 1997."),
 new Card("Natalie Portman", 71, 94, 8, "Is trained in ballet, jazz, and tap dancing."),
 new Card ("Viola Davis", 94, 115, 344, "Named one of Time magazine's 100 Most Influential People in the World in 2012."),
@@ -31,96 +31,96 @@ new Card("Scarlett Johansson", 76, 70, 27, "Suffers from a morbid fear of birds 
 new Card("Keanu Reeves", 111, 13, 56, "Loves ballroom dancing."),
 new Card("Hugh Jackman", 66, 33, 113, "Got a degree in journalism before turning to acting."),
 ]
+
 const playerHand = document.getElementById("player-hand");
 const computerHand = document.getElementById("computer-hand");
 const startButton = document.getElementById("start-button");
+const categoryButtons = document.querySelectorAll(".category-button");
 let playerCards = [];
 let computerCards = [];
+let score = 0;
 
 
-function shuffle (cards) {
-for (let i = cards.length - 1; i > 0; i--) {
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    let shuffledCards = cards[i];
-    cards[i] = cards[j];
-    cards[j] = shuffledCards;
+    let shuffledDeck = deck[i];
+    deck[i] = deck[j];
+    deck[j] = shuffledDeck;
   }
-  return cards;
+  return deck;
 }
 
-function displayHands() {
-    playerHand.innerHTML = ``;
-    computerHand.innerHTML =``;
-    for (let i = 0; i < playerCards.length; i++) {
-        playerHand.innerHTML +=`
-        <div class="card">
-      <h2>${playerCards[i].cardName}</h2>
-      <ul>
-        <li>Roles: ${playerCards[i].roles}</li>
-        <li>Awards: ${playerCards[i].awards}</li>
-        <li>IMDB score: ${playerCards[i].imdbScore}</li>
+function displayCards(playerCard, computerCard) {
+  playerHand.innerHTML = ``;
+  computerHand.innerHTML = ``;
+  
+    playerHand.innerHTML += `
+      <div class="card">
+    <h2>${playerCard.cardName}</h2>
+    <ul>
+      <li>Roles: ${playerCard.roles}</li>
+      <li>Awards: ${playerCard.awards}</li>
+      <li>IMDB score: ${playerCard.imdbScore}</li>
+    </ul>
+    <p>Fun fact: ${playerCard.funFact}</p>
+    </div>
+  `;
+
+    computerHand.innerHTML += `
+  <div class="card">
+    <h2>${computerCard.cardName}</h2>
+    <ul>
+      <li>Roles: ${computerCard.roles}</li>
+      <li>Awards: ${computerCard.awards}</li>
+      <li>IMDB score: ${playerCard.imdbScore}</li>
       </ul>
-      <p>Fun fact: ${playerCards[i].funFact}</p>
-      </div>
-    `;
-  
-    computerHand.innerHTML +=`
-    <div class="card">
-      <h2>${computerCards[i].cardName}</h2>
-      <ul>
-        <li>Roles: ${computerCards[i].roles}</li>
-        <li>Awards: ${computerCards[i].awards}</li>
-        <li>IMDB score: ${playerCards[i].imdbScore}</li>
-        </ul>
-        <p>Fun fact: ${playerCards[i].funFact}</p>
-      </div>
-    `;
+      <p>Fun fact: ${playerCard.funFact}</p>
+    </div>
+  `;
+}
+
+startButton.addEventListener("click", () => {
+  const shuffledDeck = shuffle(deck);
+  for (let i = 0; i < shuffledDeck.length; i++) {
+    i % 2 === 0
+      ? playerCards.push(shuffledDeck[i])
+      : computerCards.push(shuffledDeck[i]);
+  }
+  displayCards(playerCards[0]);
+});
+
+
+categoryButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const selectedCategory = button.textContent.trim();
+
+    const playerValue = playerCards[0][selectedCategory];
+    const computerValue = computerCards[0][selectedCategory];
+
+    if (playerValue > computerValue) {
+      playerCards.push(computerCards[0]);
+      computerCards.pop();
+      displayCards(playerCards[0], computerCards[0] /*, activePlayer*/);
+      document.getElementById('playerCards').innerHTML = "Score: " + score;
+    } else if (computerValue > playerValue) {
+      computerCards.push(playerCards[0]);
+      playerCards.pop();
+      displayCards(playerCards[0], computerCards[0] /*, activePlayer*/);
+    } else {
+      playerCards.shift();
+      computerCards.shift();
     }
+
+    if (playerCards.length === 0) {
+      console.log("Game over. You lose!");
+    } else if (computerCards.length === 0) {
+      console.log("Game over. You win!");
+    } else {
+
+      console.log("Player cards" + playerCards.length);
+
+      /*displayCards(playerCards[currentPlayCard], computerCards[currentComCard])*/
     }
-
-
-  startButton.addEventListener("click", () => {
-    const shuffledCards = shuffle(cards);
-    for (let i = 0; i < shuffledCards.length; i++) {
-      i % 2 === 0 ? playerCards.push(shuffledCards[i]) : computerCards.push(shuffledCards[i]);
-      };
-      displayHands();
-    });
-
-  
-  
-  const categoryButtons = document.querySelectorAll(".category-button");
-  categoryButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const selectedCategory = button.textContent.trim();
-  
-     
-      const playerValue = playerCards[0][selectedCategory];
-      const computerValue = computerCards[0][selectedCategory];
-
-      
-  
-      if (playerValue > computerValue) {
-        playerCards.push(computerCards[0]);
-        computerCards.shift();
-      
-  
-      } else if (computerValue > playerValue) {
-        computerCards.push(playerCards[0]);
-        playerCards.shift();
-    
-      } else {
-        playerCards.shift();
-        computerCards.shift();
-        
-      }
-  
-      if (playerCards.length === 0) {
-        console.log("Game over. You lose!");
-      } else if (computerCards.length === 0) {
-        console.log("Game over. You win!");
-      } else {
-        displayHands();
-      }
-    });
   });
+});
