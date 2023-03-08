a049ebbaf7019fc097a884ec4b777e4c541febb3
Sophie White <sophie.white@mordrid.net>
2023-03-08 18:30:56 +0100

    fix gameplay issue, create game over function

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 class Card{
    
-  constructor(cardName, roles, awards, imdbScore, funFact){
+  constructor(cardName, roles, awards, nominations, funFact){
       this.cardName = cardName
       this.roles = roles
       this.awards = awards
-      this.imdbScore = imdbScore
+      this.nominations = nominations
       this.funFact = funFact
   } 
   
   }
 
 const deck =[
-new Card("George Clooney", 81, 95, 94, "Was voted `Sexiest Man Alive` by People magazine in 1997."),
-new Card("Natalie Portman", 71, 94, 8, "Is trained in ballet, jazz, and tap dancing."),
-new Card ("Viola Davis", 94, 115, 244, "Named one of Time magazine's 100 Most Influential People in the World in 2012."),
-new Card ("Kate Winslet", 70, 104, 104, "Her performance in Eternal Sunshine of the Spotless Mind (2004) is her personal favorite."),
-new Card("Anthony Hopkins", 147, 71, 127, "His early ambition was to be a concert pianist."),
-new Card("Meryl Streep", 94, 178, 113, "As of 2018, she is the most nominated actress with 21 Academy Award nominations."),
-new Card("Joaquin Pheonix", 57, 71, 92, "He is the second actor to win an Academy Award for playing a comic book character."),
-new Card("Sandra Bullock", 59, 81, 13, "Hates musicals and vows to never participate in one."),
-new Card("Daniel Day-Lewis", 30, 148, 114, "Has dual citizenship between the United Kingdom and Ireland."),
-new Card("Julianne Moore", 107, 116, 258, "Received a star on the Hollywood Walk of Fame on October 3, 2013."),
-new Card("Samuel L. Jackson", 204, 47, 90, "Was an usher at Dr. Martin Luther King, Jr.'s funeral."),
-new Card("Glenn Close", 97, 74, 471, "Has won three Tony Awards for her work on Broadway."),
-new Card("Dustin Hoffman", 87, 64, 240, "He was considered for the role of Michael Corleone in The Godfather (1972)."),
-new Card("Salma Hayek", 85, 20, 22, "Salma fluently speaks Arabic, Spanish, Portuguese and English."),
-new Card("Tom Holland", 39, 25, 2, "Has a very strong fear of spiders"),
-new Card("McKenna Grace", 74, 3, 54, "Is a vegetarian and an animal rights activist."),
-new Card("Leonardo DiCaprio", 52, 102, 18, "He is an environmental conservationist and often advocates and supports natural causes."),
-new Card("Scarlett Johansson", 76, 70, 27, "Suffers from a morbid fear of birds and cockroaches."),
-new Card("Keanu Reeves", 111, 13, 56, "Loves ballroom dancing."),
-new Card("Hugh Jackman", 66, 33, 113, "Got a degree in journalism before turning to acting."),
+new Card("George Clooney", 81, 95, 206, "Was voted `Sexiest Man Alive` by People magazine in 1997."),
+new Card("Natalie Portman", 71, 94, 155, "Is trained in ballet, jazz, and tap dancing."),
+new Card("Viola Davis", 94, 115, 230, "Named one of Time magazine's 100 Most Influential People in the World in 2012."),
+new Card("Kate Winslet", 70, 104, 178, "Her performance in Eternal Sunshine of the Spotless Mind (2004) is her personal favorite."),
+new Card("Anthony Hopkins", 147, 71, 128, "His early ambition was to be a concert pianist."),
+new Card("Meryl Streep", 94, 178, 376, "As of 2018, she is the most nominated actress with 21 Academy Award nominations."),
+new Card("Joaquin Pheonix", 57, 71, 190, "He is the second actor to win an Academy Award for playing a comic book character."),
+new Card("Sandra Bullock", 59, 81, 122, "Hates musicals and vows to never participate in one."),
+new Card("Daniel Day-Lewis", 30, 148, 95, "Has dual citizenship between the United Kingdom and Ireland."),
+new Card("Julianne Moore", 107, 116, 163, "Received a star on the Hollywood Walk of Fame on October 3, 2013."),
+new Card("Samuel L. Jackson", 204, 47, 95, "Was an usher at Dr. Martin Luther King, Jr.'s funeral."),
+new Card("Glenn Close", 97, 74, 125, "Has won three Tony Awards for her work on Broadway."),
+new Card("Dustin Hoffman", 87, 64, 54, "He was considered for the role of Michael Corleone in The Godfather (1972)."),
+new Card("Salma Hayek", 85, 20, 51, "Salma fluently speaks Arabic, Spanish, Portuguese and English."),
+new Card("Tom Holland", 39, 25, 35, "Has a very strong fear of spiders"),
+new Card("McKenna Grace", 74, 3, 11, "Is a vegetarian and an animal rights activist."),
+new Card("Leonardo DiCaprio", 52, 102, 263, "He is an environmental conservationist and often advocates and supports natural causes."),
+new Card("Scarlett Johansson", 76, 70, 236, "Suffers from a morbid fear of birds and cockroaches."),
+new Card("Keanu Reeves", 111, 13, 36, "Loves ballroom dancing."),
+new Card("Hugh Jackman", 66, 33, 107, "Got a degree in journalism before turning to acting."),
 ]
 
 const playerHand = document.getElementById("player-hand");
@@ -62,7 +62,7 @@ function displayCards(playerCard, computerCard) {
     <ul>
       <li>Roles: ${playerCard.roles}</li>
       <li>Awards: ${playerCard.awards}</li>
-      <li>IMDB score: ${playerCard.imdbScore}</li>
+      <li>Nominations: ${playerCard.nominations}</li>
     </ul>
     <p>Fun fact: ${playerCard.funFact}</p>
     <p>Cards in your hand: ${playerCards.length}</p>
@@ -75,7 +75,7 @@ function displayCards(playerCard, computerCard) {
     <ul>
       <li>Roles: ${computerCard.roles}</li>
       <li>Awards: ${computerCard.awards}</li>
-      <li>IMDB score: ${computerCard.imdbScore}</li>
+      <li>IMDB score: ${computerCard.nominations}</li>
       </ul>
       <p>Fun fact: ${computerCard.funFact}</p>
       <p>Cards in your hand: ${computerCards.length}</p>
@@ -83,7 +83,58 @@ function displayCards(playerCard, computerCard) {
   `;
 }
 
-startButton.addEventListener("click", () => {
+function endGame(playerCards, computerCards) {
+  if (playerCards.length > 0 && computerCards.length > 0) {
+    // display the first card from each player's hand
+    displayCards(playerCards[0], computerCards[0]);
+  } else {
+    let winner = "";
+    if (playerCards.length === 0) {
+      winner = "Computer";
+    } else if (computerCards.length === 0) {
+      winner = "Player";
+    }
+    // display the game over message with the winner
+    playerHand.innerHTML = `
+      <div class="game-over">
+        <h2>Game Over</h2>
+        <p>Winner: ${winner}</p>
+        <button id="restart-button">Restart</button>
+      </div>
+    `;
+    computerHand.innerHTML = "";
+    // add a click event listener to the restart button
+    document.querySelector("#restart-button").addEventListener("click", () => {
+      // reload the page
+      location.reload();
+    });
+  }
+}
+
+function comparingHands(playerValue, computerValue){
+    if (playerValue > computerValue) {
+      playerCards.push(computerCards.shift());
+      playerCards.push(playerCards.shift());
+      console.log("You won this round")
+      endGame(playerCards, computerCards);
+      
+    } else if (computerValue > playerValue) {
+      computerCards.push(playerCards.shift());
+      computerCards.push(computerCards.shift());
+      console.log("The computer won this round")
+      endGame(playerCards, computerCards);     
+    } else {
+      console.log("It's a tie!")
+      playerCards.push(playerCards.shift());
+      computerCards.push(computerCards.shift());
+      endGame(playerCards, computerCards);
+
+    }
+  }
+
+
+
+    startButton.addEventListener("click", () => {
   const shuffledDeck = shuffle(deck);
   for (let i = 0; i < shuffledDeck.length; i++) {
     i % 2 === 0
@@ -91,50 +142,16 @@ startButton.addEventListener("click", () => {
       : computerCards.push(shuffledDeck[i]);
   }
   displayCards(playerCards[0], computerCards[0]);
+  document.querySelector('#start-button').disabled = true;
 });
 
 
 categoryButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const selectedCategory = button.value;
-
+ console.log(playerCards[index], computerCards[index])
     const playerValue = playerCards[index][selectedCategory];
     const computerValue = computerCards[index][selectedCategory];
-
-    console.log(selectedCategory, playerValue, computerValue)
-
-    if (playerValue > computerValue) {
-      displayCards(playerCards[0], computerCards[0]);
-      playerCards.push(computerCards.shift());
-      alert("You won this round")
-      
-    } else if (computerValue > playerValue) {
-      displayCards(playerCards[0], computerCards[0]);
-      computerCards.push(playerCards.shift());
-      alert("The computer won this round")
-     
-    } else {
-      displayCards(playerCards[0], computerCards[0]);
-      playerCards.shift();
-      computerCards.shift();
-      alert("It's a tie!")
-    }
-    index++;
-
-    if ((index < playerCards.length) && (index < computerCards.length)){
-      displayCards(playerCards[index], computerCards[index]);
-  
-    }
-
-    if (playerCards.length === 0) {
-      alert("Game over. You lose!");
-    } else if (computerCards.length === 0) {
-      alert("Game over. You win!");
-    } else {
-
-      console.log("You have " + playerCards.length + " cards.");
-
-    }
-  
+comparingHands(playerValue, computerValue);
   });
 });
